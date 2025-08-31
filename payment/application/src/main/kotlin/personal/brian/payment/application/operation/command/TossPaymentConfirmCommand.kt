9532d766b2eb6848218f51d3349364eb0e440841
BrianDYKim <18k7102dy@gmail.com>
2025-08-31 17:02:41 +0900

    fix: 결제 승인 요청 커맨드에 self validation 적용

--- payment/application/src/main/kotlin/personal/brian/payment/application/operation/command/TossPaymentConfirmCommand.kt
+++ payment/application/src/main/kotlin/personal/brian/payment/application/operation/command/TossPaymentConfirmCommand.kt
@@ -1,16 +1,29 @@
 package personal.brian.payment.application.operation.command
 
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import personal.brian.payment.application.dto.TossPaymentConfirmDto
+import personal.brian.springExtension.validation.SelfValidating
 
 /**
  * @author Doyeop Kim
  * @since 2025. 8. 31.
  */
 data class TossPaymentConfirmCommand(
+    @field:NotNull
+    @field:NotEmpty
     val paymentKey: String,
+    @field:NotNull
+    @field:NotEmpty
     val orderId: String,
+    @field:NotNull
+    @field:NotEmpty
     val amount: String,
-) {
+) : SelfValidating<TossPaymentConfirmCommand>() {
+    init {
+        validateSelf()
+    }
+
     companion object {
         fun from(request: TossPaymentConfirmDto.Request): TossPaymentConfirmCommand {
             return TossPaymentConfirmCommand(
